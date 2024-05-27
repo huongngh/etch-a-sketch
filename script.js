7ddc6b2739f97a2bd3cf56d5c997fdc9a1e9cd9a
huonghuynh <e.huonghuynh@gmail.com>
2024-05-27 16:30:29 +0700

    Create 16*16 grid items using JavaScript

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const container = document.querySelector("container");
+const container = document.querySelector('.container');
+console.log(container);
 
 var  square  = 16*16;
 for (let i = 0; i<square; i++){
-    const grid = document.createElement("div");
-    grid.addClassList = "grid";
-    grid.textContent = i;
+    var gridItem = document.createElement('div');
+    gridItem.classList.add('gridItem');
+    gridItem.textContent = i;
+    console.log(gridItem);
+    container.appendChild(gridItem);
 }
-
-container.appendChild(grid);
