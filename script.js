7bbc511aa5ad4e56840039738ed24831f8a65513
huonghuynh <e.huonghuynh@gmail.com>
2024-05-28 14:25:18 +0700

    Delete grid text content

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var  square  = 16*16;
 for (let i = 0; i<square; i++){
     var gridItem = document.createElement('div');
     gridItem.classList.add('gridItem');
-    gridItem.textContent = i;
+    gridItem.textContent = '';
     console.log(gridItem);
     container.appendChild(gridItem);
 }
+
 const gridItems = document.querySelectorAll('.gridItem');
 gridItems.forEach(gridItem =>{
     gridItem.addEventListener('mouseover',()=>{
